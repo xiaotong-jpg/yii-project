37294a48ccb970c4a3fd6d6156b042e8c9a0dc86
王皓然(Wang Haoran) <166805401+lioritliorit@users.noreply.github.com>
2025-12-16 14:58:46 +0800

    Add files via upload

---
+++ common/models/ArticleSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace common\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Article;
+
+/**
+ * ArticleSearch represents the model behind the search form of `common\models\Article`.
+ */
+class ArticleSearch extends Article
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'status'], 'integer'],
+            [['title', 'cover_image', 'content', 'author', 'publish_date', 'source_url'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Article::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'publish_date' => $this->publish_date,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'cover_image', $this->cover_image])
+            ->andFilterWhere(['like', 'content', $this->content])
+            ->andFilterWhere(['like', 'author', $this->author])
+            ->andFilterWhere(['like', 'source_url', $this->source_url]);
+
+        return $dataProvider;
+    }
+}
